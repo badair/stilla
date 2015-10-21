01739a82f851cef1b9af48c661fad5b6956d91b8
badair <badair@harding.edu>
2015-10-21 01:16:17 -0500

    made "where" filter lazy-evaluated

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@
 
 template <typename arg_itr, typename functor>
 struct lazy_eval_iterator : boost::iterator_facade<
-								lazy_eval_iterator<arg_itr, functor>,
-								typename std::result_of<functor(arg_itr)>::type,
-								boost::single_pass_traversal_tag,
-								typename std::add_lvalue_reference<typename std::result_of<functor(arg_itr)>::type>::type,
-								std::ptrdiff_t
-							>
+	lazy_eval_iterator<arg_itr, functor>,
+	typename std::result_of<functor(arg_itr)>::type,
+	boost::single_pass_traversal_tag,
+	typename std::add_lvalue_reference<typename std::result_of<functor(arg_itr)>::type>::type,
+	std::ptrdiff_t
+>
 {
 	using expr_type = functor;
 	using this_type = lazy_eval_iterator<arg_itr, functor>;
@@ -109,6 +109,91 @@ lazy_eval_iterator<arg_itr, functor> make_lazy_eval_iterator(arg_itr itr, functo
 	return lazy_eval_iterator<arg_itr, functor>(itr, f);
 }
 
+template <typename arg_itr, typename functor>
+struct lazy_filter_iterator : boost::iterator_facade<
+	lazy_filter_iterator<arg_itr, functor>,
+	typename std::result_of<functor(arg_itr)>::type,
+	boost::single_pass_traversal_tag,
+	typename std::add_lvalue_reference<typename std::result_of<functor(arg_itr)>::type>::type,
+	std::ptrdiff_t
+>
+{
+	using expr_type = functor;
+	using this_type = lazy_filter_iterator<arg_itr, functor>;
+
+	mutable arg_itr arg; // :(
+	functor expr;
+	arg_itr end;
+	using value_type = decltype(*arg);
+
+	lazy_filter_iterator(arg_itr arg_, arg_itr end_, functor expr_) : arg(arg_), end(end_), expr(expr_)
+	{
+		increment_to_next_match();
+	}
+
+	lazy_filter_iterator(const this_type& other) : arg(other.arg), end(other.end), expr(other.expr)
+	{
+	}
+
+	void increment_to_next_match() const
+	{
+		while (arg != end && !expr(arg))
+		{
+			++arg;
+		}
+	}
+
+	value_type& operator*() const
+	{
+		auto& result = *arg;
+
+		increment_to_next_match();
+
+		return *arg;
+	}
+
+	inline this_type& operator++()
+	{
+		++arg;
+		return *this;
+	}
+
+	inline this_type operator++(int)
+	{
+		auto result = this_type(arg, expr);
+		++arg;
+		return result;
+	}
+
+	inline bool operator!=(const this_type& other) const
+	{
+		return arg != other.arg;
+	}
+
+	//const - whatever
+	value_type& dereference() const
+	{
+		return **this;
+	}
+
+	bool equal(this_type other) const
+	{
+		return arg == other.arg;
+	}
+
+	auto& increment() const
+	{
+		return ++(*this);
+	}
+};
+
+template <typename arg_itr, typename functor>
+lazy_filter_iterator<arg_itr, functor> make_lazy_filter_iterator(arg_itr itr, arg_itr end, functor f)
+{
+	return lazy_filter_iterator<arg_itr, functor>(itr, end, f);
+}
+
+
 template<typename itr>
 struct basic_range
 {
@@ -129,7 +214,7 @@ struct basic_range
 	{
 		return _end;
 	}
-	
+
 	auto& first()
 	{
 		return *_begin;
@@ -140,11 +225,15 @@ struct basic_range
 		auto count = 0;
 		auto temp = _begin;
 
-		while (temp != _end)
+		do
 		{
 			++temp;
-			++count;
-		}
+
+			if (temp != _end)
+			{
+				++count;
+			}
+		} while (temp != _end);
 
 		return count;
 	}
@@ -173,48 +262,47 @@ struct basic_range
 		return optional_type();
 	}
 
-	auto& to_vector()
+	auto to_vector()
 	{
-		auto result = vector<typename std::remove_reference<value_type>::type>(count());
-		std::copy(begin(), end(), std::back_inserter<decltype(result)>(result));
-		return std::move(result);
+		return std::move(std::make_unique<std::vector<typename std::remove_reference<value_type>::type> >(begin(), end()));
 	}
 
-	template<typename select_funtor>
-	auto select_(select_funtor f) -> decltype(make_basic_lazy_range(_begin, _end, f))
+	template<typename select_functor>
+	auto stilla_s3l3ct(select_functor f) -> decltype(make_basic_lazy_range(_begin, _end, f))
 	{
 		return make_basic_lazy_range(_begin, _end, f);
 	}
 
-	//todo
-	//template<typename select_funtor>
-	//auto select_many(select_funtor f) -> decltype(make_basic_lazy_range(_begin, _end, f))
-	//{
-	//	return make_basic_lazy_range(_begin, _end, f);
-	//}
-
 	template<typename where_functor>
-	auto where_(where_functor f)
+	auto stilla_wh3r3(where_functor f)
 	{
-		//todo: implement my own version of this so it can be lazy - coroutines?
-		auto filtered_begin = boost::make_filter_iterator<decltype(f), itr>(f, _begin, _end);
-		auto filtered_end = boost::make_filter_iterator<decltype(f), itr>(f, _end, _end);
+		auto filtered_begin = make_lazy_filter_iterator(_begin, _end, f);
+		auto filtered_end = make_lazy_filter_iterator(_end, _end, f);
 		return basic_range<decltype(filtered_begin)>(filtered_begin, filtered_end);
 	}
 
 	template<typename functor>
-	auto for_each_(functor f) const
+	auto stilla_f0r_34ch(functor f) const
 	{
 		std::for_each(_begin, _end, f);
 		return *this;
 	}
 
-	template<typename functor>
-	auto call_once_(functor f) const
+	/* TODO
+	template<typename range_t>
+	auto concat(range_t r) const
 	{
-		f();
-		return *this;
-	}
+	static_assert(std::is_same<decltype()*r.begin())), decltype(*_begin)>::value, "Concatenating range has a different underlying value." );
+
+	return *this;
+	}*/
+
+	/*TODO (do concat first)
+	template<typename select_funtor>
+	auto select_many(select_funtor f) -> decltype(make_basic_lazy_range(_begin, _end, f))
+	{
+	return make_basic_lazy_range(_begin, _end, f);
+	}*/
 
 	bool has_any()
 	{
@@ -222,13 +310,13 @@ struct basic_range
 	}
 
 	template<typename functor>
-	bool none_(functor f)
+	bool stilla_n0n3(functor f)
 	{
-		return !any_<functor>(f);
+		return !stilla_4ny<functor>(f);
 	}
 
 	template<typename functor>
-	bool any_(functor f)
+	bool stilla_4ny(functor f)
 	{
 		for (auto& x : *this)
 		{
@@ -239,6 +327,20 @@ struct basic_range
 		}
 
 		return false;
+	}
+
+	template<typename functor>
+	bool stilla_4ll(functor f)
+	{
+		for (auto& x : *this)
+		{
+			if (!f(x))
+			{
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	template<typename other>
@@ -302,14 +404,14 @@ using std::array;
 using std::string;
 
 //you can format these macros however you prefer.
-#define select(itr, expr) select_([&](const auto& __itr){auto& itr = *__itr; return (expr);})
-#define where(itr, expr) where_([&](const auto& itr){return (expr);})
+#define select(element, expr) stilla_s3l3ct([&](const auto& __itr){const auto& element = *__itr; return (expr);})
+#define where(element, expr) stilla_wh3r3([&](const auto& __itr){const auto& element = *__itr; return (expr);})
 #define from(container) make_basic_range(container.begin(), container.end())
-#define for_each(itr, expr) for_each_([&](const auto& itr){(expr);})
-#define call_once(expr) call_once_([&]{(expr);})
-#define any(itr, expr) any_([&](const auto& itr){return (expr);})
-#define none(itr, expr) none_([&](const auto& itr){return (expr);})
-#define first_where(itr, expr) optional_first([&](const auto& itr){return (expr);})
+#define for_each(element, expr) stilla_f0r_34ch([&](const auto& element){(expr);})
+#define any(element, expr) stilla_4ny([&](const auto& element){return (expr);})
+#define all(element, expr) stilla_4ll([&](const auto& element){return (expr);})
+#define none(element, expr) stilla_n0n3([&](const auto& element){return (expr);})
+#define first_or_default_where(element, expr) optional_first([&](const auto& element){return (expr);})
 #define first_or_default() optional_first()
 
 struct foo
@@ -332,43 +434,29 @@ string uppercase(string s)
 int main()
 {
 	auto foos = vector<foo>{ foo(0), foo(1), foo(2), foo(3), foo(4) };
-	auto strings = array<string, 5>{ "bob", "sally", "john", "cindy", "mark"};
+	auto strings = array<string, 6>{ "bob", "sally", "john", "cindy", "mark"};
 
-	auto johnless = from(foos)
-				    .for_each(f, cout << "foo numbers: " << f.number << endl)
-				    .select(f, strings[f.number])
-				    .for_each(s, cout << "strings: " << s << endl)
-				    .where(s, s != "john");
-
-	auto john_string =   from(strings)
-						 .first_where(s, !johnless.contains(s));
-
-	cout << "john string: " << john_string << endl;
-				 
-                 
-
-	cout << endl << "count: " << johnless.count() << endl << endl;
-
-	for (auto element : johnless)
-	{
-		cout << element << endl;
-	}
-
-	auto vec = johnless.to_vector();
-
-	for (auto element : vec)
-	{
-		cout << element << endl;
-	}
+	auto range = from(foos)
+                .select(f, strings[f.number])
+                .where(s, s != "john")
+                .select(s, uppercase(s))
+                .for_each(s, cout << s << " starts with " << from(foos).select(f, strings[f.number]).where(s2, s[0] == ::toupper(s2[0])).select(s2, s2[0]).first() << endl);
 
 	cout << endl;
 
-	//without macros
-	auto ugly_range = make_basic_range(foos.begin(), foos.end())
-                        .select_([&](const auto& x) {return strings[(*x).number]; })
-                        .where_([&](const auto& s) {return s != "john"; })
-                        .select_([&](const auto& s) {return uppercase(*s); })
-                        .for_each_([&](const auto& s) {cout << s << endl; });
+	for (auto element : range)
+	{
+		cout << element << endl;
+	}
+
+	cout << endl << "count: " << range.count() << endl << endl;
+
+	auto vec = *(range.to_vector());
+
+	for (size_t i = 0; i < vec.size(); ++i)
+	{
+		cout << vec[i] << endl;
+	}
 
 	cin.get();
 	return 0;
